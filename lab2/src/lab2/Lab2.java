81da1b6f9de208a0bf3eea3450f2e6fe8d2fc553
n-thanh <thban2803@gmail.com>
2023-10-15 15:16:43 +0900

    done lab 2 - bai 1

--- lab2/src/lab2/Lab2.java
+++ lab2/src/lab2/Lab2.java
@@ -4,17 +4,44 @@
  */
 package lab2;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
 /**
  *
  * @author admin
  */
 public class Lab2 {
 
+    private static void bai1() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập hệ số a, b của phương trình bậc nhất: ");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phương trình có vô số nghiệm");
+            } else {
+                System.out.println("Phương trình vô nghiệm");
+            }
+        } else {
+            System.out.printf("Nghiệm của phương trình là: %f%n", -b / a);
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            throw new InternalError("VM does not support mandatory encoding UTF-8");
+        }
+        Lab2.bai1();
     }
-    
 }
